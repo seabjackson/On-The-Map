665a812e3d91d5c22e8bce5ec623c9a69c9b7e7e
Seab <honeyboyseab@gmail.com>
2016-07-28 15:10:22 -0400

    added the login viewcontroller

---
+++ On The Map/On The Map/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by lily on 7/27/16.
+//  Copyright © 2016 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
