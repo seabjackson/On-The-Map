1684280aac0088f463ba2f74ed0b37aa1b39c09d
Seab <honeyboyseab@gmail.com>
2016-08-17 14:42:26 -0400

    added UI elements for information post view

--- On The Map/On The Map/PostingViewController.swift
+++ On The Map/On The Map/PostingViewController.swift
@@ -7,20 +7,92 @@
 //
 
 import UIKit
+import MapKit
 
-class PostingViewController: UIViewController {
+class PostingViewController: UIViewController, MKMapViewDelegate {
+
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var findButton: UIButton!
+    @IBOutlet weak var locationView: UIView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var linkTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+    
+    var annotations = [MKPointAnnotation]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        mapView.hidden = true
+        linkTextField.hidden = true
+        locationLabel.hidden = false
+        
+        
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var cancel: UIButton!
+    
+    @IBAction func findOnMap(sender: UIButton) {
+        mapView.hidden = false
+        locationLabel.hidden = true
+        locationView.hidden = true
+        findButton.hidden = true
+        submitButton.hidden = false
+        linkTextField.hidden = false
+        
+        if let location = textField.text {
+            CLGeocoder().geocodeAddressString(location) { (placemarks, error) in
+                if error != nil {
+                    print(error)
+                    return
+                }
+                if placemarks?.count > 0 {
+                    let placemark = placemarks?[0]
+                    let location = placemark?.location
+                    let coordinate = location?.coordinate
+                    print("\nlat: \(coordinate!.latitude), long: \(coordinate!.longitude)")
+                    if placemark?.areasOfInterest?.count > 0 {
+                        let areaOfInterest = placemark!.areasOfInterest![0]
+                        print(areaOfInterest)
+                    } else {
+                        print("No area of interest found")
+                    }
+                    
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate  = coordinate!
+                    self.annotations.append(annotation)
+                    self.mapView.addAnnotations(self.annotations)
+                    
+                }
+            }
+        }
     }
     
+
+
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = UIColor.redColor()
+            pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
 
     /*
     // MARK: - Navigation
